ef516ec843cd3a6d59f4c1009089686d84181e74
Felix Brix <felix.brix@onlinehome.de>
2015-05-11 10:21:53 +0200

    Changed routing to a better way and added view for boards controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Overrides urls for devise paths
+  # Taken from http://stackoverflow.com/a/3845245 | doritostains | 11th May 2015, 10:12 am
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  # Sets additional urls for devise paths
-  # Taken from http://stackoverflow.com/a/3827783 | shingara | 11th May 2015, 09:28 am
-  devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-    get 'logout', to: 'devise/sessions#destroy'
-    get 'register', to: 'devise/registrations#new'
-  end
-
-  root 'dashboard#index'
+  root 'boards#index'
 end
