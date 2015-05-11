5f2a1a888732d1b7414fbf49fa1c6b24d448cd3f
Felix Brix <felix.brix@onlinehome.de>
2015-05-11 19:44:34 +0200

    Added not found and wrong path handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  rescue_from ActiveRecord::RecordNotFound,   with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
+
+  def raise_routing_error
+    raise ActionController::RoutingError.new params[:path]
+  end
+
+  private
+
+  def not_found
+    render file: "#{Rails.root}/public/404"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # Taken from http://stackoverflow.com/a/3845245 | doritostains | 11th May 2015, 10:12 am
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
+  match '*path', to: 'application#raise_routing_error', via: :all
+
   root 'boards#index'
 end
