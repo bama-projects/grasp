1ee19016210a1a866bad8a5ecb03bafb31212f7e
Felix Brix <felix.brix@onlinehome.de>
2015-05-12 08:02:02 +0200

    Added username migration and model constrains

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Regexp checks for an initial character and a length of at least three
+  # Taken from http://stackoverflow.com/a/6102146 | Shea Levy | 12th May 2015, 7:56 am
+  validates :username, presence: true, uniqueness: true, format: { with: /[a-zA-Z][a-zA-Z0-9_]{2,}/,
+                                                         message: 'only allows letters and numbers starting with a character.' }
 end
