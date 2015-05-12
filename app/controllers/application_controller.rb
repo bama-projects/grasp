c15307740af513cc7412fc4cd25ce6e2b270e621
Felix Brix <felix.brix@onlinehome.de>
2015-05-12 10:50:41 +0200

    Removed unneeded parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   # Taken from http://stackoverflow.com/a/21486039 | SNEH PANDYA | 11th May 2015, 8:17 am
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :avatar) }
   end
 end
