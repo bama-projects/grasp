bba995160b894647a27cdf801b11b694477ef665
Felix Brix <felix.brix@onlinehome.de>
2015-06-08 11:19:02 +0200

    Added adjusted schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513090208) do
+ActiveRecord::Schema.define(version: 20150608085743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "boards", force: :cascade do |t|
+  create_table "courses", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
@@ -27,13 +27,13 @@ ActiveRecord::Schema.define(version: 20150513090208) do
     t.datetime "submission_until"
   end
 
-  create_table "boards_users", id: false, force: :cascade do |t|
-    t.integer "board_id"
+  create_table "courses_users", id: false, force: :cascade do |t|
+    t.integer "course_id"
     t.integer "user_id"
   end
 
-  add_index "boards_users", ["board_id", "user_id"], name: "index_boards_users_on_board_id_and_user_id", using: :btree
-  add_index "boards_users", ["user_id"], name: "index_boards_users_on_user_id", using: :btree
+  add_index "courses_users", ["course_id", "user_id"], name: "index_courses_users_on_course_id_and_user_id", using: :btree
+  add_index "courses_users", ["user_id"], name: "index_courses_users_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
